1ce7b3071bcb38da8ad12075b72210a7347ea9a9
heddle <david.heddle@cnu.edu>
2021-03-17 15:13:11 -0400

    initial import

---
+++ bCNU/src/main/java/cnuphys/bCNU/component/rangeslider/IRangeSliderListener.java
@@ -0,0 +1,13 @@
+package cnuphys.bCNU.component.rangeslider;
+
+import java.util.EventListener;
+
+public interface IRangeSliderListener extends EventListener {
+
+	/**
+	 * The range has changed.
+	 * 
+	 * @param slider the RangeSlider being updated.
+	 */
+	public void rangeSliderChanged(RangeSlider slider);
+}
---
+++ bCNU/src/main/java/cnuphys/bCNU/simanneal/IAnneal.java
@@ -0,0 +1,12 @@
+package cnuphys.bCNU.simanneal;
+
+public interface IAnneal {
+
+	/**
+	 * This is the metropolis algorithm getEnergy method
+	 * 
+	 * @return the "energy"
+	 */
+	public double getEnergy();
+
+}
