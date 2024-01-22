18ca942778c3c7232d6011e8da425fd943ba21b9
David Heddle <dheddle@circadence.com>
2024-01-22 17:23:57 -0500

    new scanner

--- ced/src/main/java/cnuphys/ced/event/ScanManager.java
+++ ced/src/main/java/cnuphys/ced/event/ScanManager.java
@@ -1,5 +1,44 @@
 package cnuphys.ced.event;
 
-public class ScanManager {
+import java.util.ArrayList;
+
+import org.jlab.io.base.DataEvent;
+
+import cnuphys.ced.clasio.ClasIoEventManager;
+import cnuphys.ced.clasio.ClasIoEventManager.EventSourceType;
+import cnuphys.ced.clasio.IClasIoEventListener;
+
+/**
+ * This class manages the rapid scanning of events
+ * for making as map of event index to true event number
+ */
+public class ScanManager implements IClasIoEventListener {
+	
+	// event manager
+	private ClasIoEventManager _eventManager = ClasIoEventManager.getInstance();
+
+	
+	//the map 
+	ArrayList<Integer> _eventIndexToEventNumber = new ArrayList<Integer>();
+	
+	public ScanManager() {
+		_eventManager.addClasIoEventListener(this, 1);
+	}
+
+	@Override
+	public void newClasIoEvent(DataEvent event) {
+	}
+
+	@Override
+	public void openedNewEventFile(String path) {
+		_eventIndexToEventNumber.clear();
+	}
+
+	@Override
+	public void changedEventSource(EventSourceType source) {
+		_eventIndexToEventNumber.clear();
+	}
+	
+	
 
 }
